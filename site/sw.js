c8e62a1dc1d45f3562271edd7d0c7ab4fe943111
Yarden Zamir <dev@yarden-zamir.com>
2026-09-04 21:49:52 +0300

    content: drop the dinners fact tile

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 /* Offline: precache the page and its data; cache map tiles and fonts as they are used or saved. */
-var VERSION = 'gr52-0b6879eca9f3';
+var VERSION = 'gr52-09e4eca023fe';
 var PRECACHE = ['/', '/GR52_all-in-one.gpx', '/map.js', '/vendor/leaflet.min.js', '/vendor/leaflet.min.css',
   '/vendor/images/layers.png', '/vendor/images/layers-2x.png', '/manifest.webmanifest', '/icon.svg',
   '/maps/overview.webp', '/maps/north.webp', '/maps/merv.webp', '/maps/authion.webp', '/maps/menton.webp'];
