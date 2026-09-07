a5ee8d62bf5bd1a5d3e1e6ad79f743cb6a1e747e
Yarden Zamir <dev@yarden-zamir.com>
2026-09-07 23:34:57 +0300

    feat: exposed stretches (Authion, Basto), accent colour, event wording (from template)

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 /* Offline: precache the page and its data; cache map tiles and fonts as they are used or saved. */
-var VERSION = 'gr52-10821ac114d9';
+var VERSION = 'gr52-43094c43c5bf';
 var PRECACHE = ["/", "/GR52_all-in-one.gpx", "/map.js", "/vendor/leaflet.min.js", "/vendor/leaflet.min.css", "/vendor/images/layers.png", "/vendor/images/layers-2x.png", "/manifest.webmanifest", "/icon.svg", "/maps/authion.webp", "/maps/menton.webp", "/maps/merv.webp", "/maps/north.webp", "/maps/overview.webp"];
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(VERSION).then(function (c) { return c.addAll(PRECACHE); }).then(function () { return self.skipWaiting(); }));
