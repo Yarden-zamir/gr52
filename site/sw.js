1b486cd5e79aee140b84a981ac72639939e42531
Yarden Zamir <dev@yarden-zamir.com>
2026-09-07 14:59:45 +0300

    fix: planned hours parsing for the day simulator (from template)

--- site/sw.js
+++ site/sw.js
@@ -1,5 +1,5 @@
 /* Offline: precache the page and its data; cache map tiles and fonts as they are used or saved. */
-var VERSION = 'gr52-aeab346a1423';
+var VERSION = 'gr52-09d93ff467e2';
 var PRECACHE = ["/", "/GR52_all-in-one.gpx", "/map.js", "/vendor/leaflet.min.js", "/vendor/leaflet.min.css", "/vendor/images/layers.png", "/vendor/images/layers-2x.png", "/manifest.webmanifest", "/icon.svg", "/maps/authion.webp", "/maps/menton.webp", "/maps/merv.webp", "/maps/north.webp", "/maps/overview.webp"];
 self.addEventListener('install', function (e) {
   e.waitUntil(caches.open(VERSION).then(function (c) { return c.addAll(PRECACHE); }).then(function () { return self.skipWaiting(); }));
